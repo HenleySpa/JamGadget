399ba8b48da762f1e5c829e7561d12e0d1403492
hsanmartin <hsanmartin@henley.cl>
2015-12-10 13:15:36 -0300

    Update JamOpenBO.js

--- JamOpenBO.js
+++ JamOpenBO.js
@@ -1,6 +1,9 @@
 //JS JAM OPEN BO
-
+function GETWEBIURL()
+{
   var Msj="Hola, Paso funcion 1";
   //alert("Hello! I am an alert box!!");
   alert('El mensaje es ' +Msj+ '. Y termino'); 
+}
 
+GETWEBIURL();
