e966aeab40e983a4bd057b7a2c78bf1acdfdc32b
hsanmartin <hsanmartin@henley.cl>
2015-12-10 13:28:55 -0300

    Update JamOpenBO.js

--- JamOpenBO.js
+++ JamOpenBO.js
@@ -1,9 +1,18 @@
 //JS JAM OPEN BO
 function GETWEBIURL()
 {
-  var Msj="Hola, Paso funcion 1";
-  //alert("Hello! I am an alert box!!");
-  alert('El mensaje es ' +Msj+ '. Y termino'); 
+  
+  string userName = "Administrator";
+  string password = "elegur2012";
+  string auth = "secEnterprise";       
+  string baseURL = " http://200.27.160.150:6405/biprws/";
+  string LogonURI = baseURL + "logon/long";
+  string logonToken;
+  string docURL = "http://200.27.160.150:8080/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=AdW_gcRTVzlNkzWHnJIU_tU";
+  alert('El mensaje es ' +docURL+ '. Y termino'); 
+  
+  
+  
 }
 
 GETWEBIURL();
