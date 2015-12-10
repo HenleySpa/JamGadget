d9c6540c1a6958dab00279176d625187b1d06fa1
hsanmartin <hsanmartin@henley.cl>
2015-12-10 14:31:22 -0300

    Update JamOpenBO.js

--- JamOpenBO.js
+++ JamOpenBO.js
@@ -2,14 +2,7 @@
 function GETWEBIURL()
 {
   
-  string userName = "Administrator";
-  string password = "elegur2012";
-  string auth = "secEnterprise";       
-  string baseURL = " http://200.27.160.150:6405/biprws/";
-  string LogonURI = baseURL + "logon/long";
-  string logonToken;
-  string docURL = "http://200.27.160.150:8080/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=AdW_gcRTVzlNkzWHnJIU_tU";
-  alert('El mensaje es ' +docURL); 
+ 
   alert('El mensaje es ESTA VACIO'); 
 }
 
