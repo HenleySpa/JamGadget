39a6352c4be32b01483b7bcca7f6e571bccca5fa
hsanmartin <hsanmartin@henley.cl>
2015-12-11 13:16:58 -0300

    Update JamOpenBO.js

--- JamOpenBO.js
+++ JamOpenBO.js
@@ -27,9 +27,9 @@ function gtokenlogin()
 	ptoken.send(sxml);
 	dtoken=encodeURI(ptoken.getResponseHeader('X-SAP-LogonToken'));
 	
-	alert(ptoken.responseText);
+	//alert(ptoken.responseText);
     	login=docURL+'&'+dtoken;
-	//openurl(login);
+	openurl(login);
 
 }
 
