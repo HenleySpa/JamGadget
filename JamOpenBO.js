f2a0337cc3145463daea342cb13152255ca0858a
hsanmartin <hsanmartin@henley.cl>
2015-12-11 13:01:19 -0300

    Update JamOpenBO.js

--- JamOpenBO.js
+++ JamOpenBO.js
@@ -1,7 +1,13 @@
 //JS JAM OPEN BO
 
 //Función para abrir la URL
-
+function openurl(url)
+{
+	var newwindow;
+	   	newwindow=window.open(url,'_blank', 'directories =yes, menubar =yes, status=yes, toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
+	   	//if (window.focus) {newwindow.focus()}
+	   	win.focus();
+}
 
 function gettoken()
 {
@@ -27,10 +33,4 @@ function gettoken()
 
 }
 
-function openurl(url)
-{
-	var newwindow;
-	   	newwindow=window.open(url,'_blank', 'directories =yes, menubar =yes, status=yes, toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
-	   	//if (window.focus) {newwindow.focus()}
-	   	win.focus();
-}
+
