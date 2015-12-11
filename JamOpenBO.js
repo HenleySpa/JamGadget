04084ebaa806e5bdc84b4f359c942f93a8782570
hsanmartin <hsanmartin@henley.cl>
2015-12-11 10:28:07 -0300

    Update JamOpenBO.js

--- JamOpenBO.js
+++ JamOpenBO.js
@@ -1,9 +1,4 @@
 //JS JAM OPEN BO
-// Al cargarse el archivo JavaScript, se muestra un mensaje
-//alert("Hola Mundo!");
- 
-// Despues del primer mensaje, se muestra otro mensaje seguido
-//alert("Soy el primer script");
 
 //Función para abrir la URL
 function popupwebi(url)
@@ -14,7 +9,8 @@ function popupwebi(url)
 	win.focus();
 }
 
-function getwebiurl()
+
+function gettoken()
 {
 	var userName="Administrator";
 	var password="elegur2012";
@@ -23,12 +19,17 @@ function getwebiurl()
 	var LogonURI = baseURL + "logon/long";
 	var docURL = "http://200.27.160.150:8080/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=AdW_gcRTVzlNkzWHnJIU_tU";
 	var login;
-	popupwebi(docURL);
-	//popupwebi(LogonURI);
+	var dtoken;
+	var sxml= '<?xml version="1.0"?><attrs xmlns="http://www.sap.com/rws/bip"><attr name="userName" type="string">'+userName+'</attr><attr name="password" type="string">'+password+'</attr><attr name="auth" type="string" possibilities="secEnterprise,secLDAP,secWinAD">secEnterprise</attr></attrs>';
+	var ptoken= new XMLHttpRequest();
+	ptoken.open('POST',LogonURI,false);
+	ptoken.setRequestHeader('X-PINGARUNER', 'pingpong');
+    	ptoken.setRequestHeader('Content-Type', 'application/xml');
+	ptoken.send(sxml);
+	dtoken=encodeURI(ptoken.getResponseHeader('X-SAP-LogonToken'));
+	
+	//alert(ptoken.responseText);
+    login=docURL+'&'+dtoken;
+	alert(login);
 
-
-	
-	
-	
-	
 }
