298bc7721cf9f80ff3856b671e84445812659936
hsanmartin <hsanmartin@henley.cl>
2015-12-11 12:33:08 -0300

    Update JamOpenBO.js

--- JamOpenBO.js
+++ JamOpenBO.js
@@ -4,9 +4,9 @@
 function popupwebi(url)
 {
 	var newwindow;
-   	newwindow=window.open(url,'_blank', 'directories =yes, menubar =yes, status=yes, toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
-   	//if (window.focus) {newwindow.focus()}
-   	win.focus();
+	   	newwindow=window.open(url,'_blank', 'directories =yes, menubar =yes, status=yes, toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
+	   	//if (window.focus) {newwindow.focus()}
+	   	win.focus();
 }
 
 
@@ -22,14 +22,14 @@ function gettoken()
 	var dtoken;
 	var sxml= '<?xml version="1.0"?><attrs xmlns="http://www.sap.com/rws/bip"><attr name="userName" type="string">'+userName+'</attr><attr name="password" type="string">'+password+'</attr><attr name="auth" type="string" possibilities="secEnterprise,secLDAP,secWinAD">secEnterprise</attr></attrs>';
 	var ptoken= new XMLHttpRequest();
-    	ptoken.open('POST',LogonURI,false);
-    	ptoken.setRequestHeader('X-PINGARUNER', 'pingpong');
-        ptoken.setRequestHeader('Content-Type', 'application/xml');
-    	ptoken.send(sxml);
-    	dtoken=encodeURI(ptoken.getResponseHeader('X-SAP-LogonToken'));
-	
-   	//alert(ptoken.responseText);
-        login=docURL+'&'+dtoken;
-   	popuwebi(login);
+	    	ptoken.open('POST',LogonURI,false);
+	    	ptoken.setRequestHeader('X-PINGARUNER', 'pingpong');
+	        ptoken.setRequestHeader('Content-Type', 'application/xml');
+	    	ptoken.send(sxml);
+	    	dtoken=encodeURI(ptoken.getResponseHeader('X-SAP-LogonToken'));
+		
+	   	//alert(ptoken.responseText);
+	        login=docURL+'&'+dtoken;
+	   	popuwebi(login);
 
 }
