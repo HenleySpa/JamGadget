bba928478a09c7599db4b4de6e794d100cdef126
hsanmartin <hsanmartin@henley.cl>
2015-12-11 11:27:11 -0300

    Update JamOpenBO.js

--- JamOpenBO.js
+++ JamOpenBO.js
@@ -4,6 +4,7 @@
 function gettoken()
 {
  var url="http://200.27.160.150:8080/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=AdW_gcRTVzlNkzWHnJIU_tU";
+ var newwindow;
 newwindow=window.open(url,'_blank', 'directories =yes, menubar =yes, status=yes, toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
 
 }
