ef5dd893367017413d29cf368aef6478cf6b9af4
hsanmartin <hsanmartin@henley.cl>
2015-12-11 11:22:12 -0300

    Update JamOpenBO.js

--- JamOpenBO.js
+++ JamOpenBO.js
@@ -1,5 +1,9 @@
 //JS JAM OPEN BO
 
 //Función para abrir la URL
-var url="http://200.27.160.150:8080/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=AdW_gcRTVzlNkzWHnJIU_tU";
+function gettoken()
+{
+ var url="http://200.27.160.150:8080/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=AdW_gcRTVzlNkzWHnJIU_tU";
 newwindow=window.open(url,'_blank', 'directories =yes, menubar =yes, status=yes, toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
+
+}
